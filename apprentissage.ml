020a577af3e2cadecc96a899bb3dc17778e69e0b
daothauvin <daothauvin@free.fr>
2019-11-28 16:11:57 +0100

    version 0.0.1

--- apprentissage.ml
+++ apprentissage.ml
@@ -3,14 +3,14 @@
    recommandée. Le code que vous devez écrire prend en entrée deux
    listes de mots I et E, lues à partir d'un fichier passé en argument
    et renvoie sur la sortie standard le code SMT-LIB à faire passer
-   dans une solveur SMT, comme Z3. 
+   dans une solveur SMT, comme Z3.
  *)
 
 open Printf
 
 (* ensembles de test : ensemble I *)
 let li = ["";"ab"]
-             
+
 (* ensembles de test : ensemble E *)
 let le = ["aa";"b"]
 
@@ -19,27 +19,28 @@ let le = ["aa";"b"]
 (* EXERCICE 1 : extraire  l'alphabet de l'automate.
    La fonction alphabet_from_list prend en entrée une liste de
    chaînes de caractères et retourne une liste triée de
-   caractères sans duplication. 
+   caractères sans duplication.
  *)
-(* explode : string -> char list 
-   prend une chaîne de caractères et renvoie la liste de ses caractères 
+(* explode : string -> char list
+   prend une chaîne de caractères et renvoie la liste de ses caractères
  *)
 let explode s =
   let rec exp i l =
     if i < 0 then l else exp (i - 1) (s.[i] :: l) in
   exp (String.length s - 1) []
 
-(* alphabet_from_list : string list -> char list  
-   - prend en entrée une liste l de chaînes de caractères 
+(* alphabet_from_list : string list -> char list
+   - prend en entrée une liste l de chaînes de caractères
    - renvoie la liste triée et sans duplication de caractères
    apparaissant dans l
  *)
+
 let alphabet_from_list l =
-  (* à compléter *)
-  []
-  
+  (* à compléter : explode puis flatten puis sort_uniq avec compare de char *)
+  List.sort_uniq Char.compare (List.flatten (List.map explode l))
+
 (* test *)
-(* let a = alphabet_from_list (li @ le) *)
+let a = alphabet_from_list (li @ le)
 
 (* ======================================================================= *)
 (* EXERCICE 2 : définition de l'alphabet A et de l'arbre préfixe T en
@@ -48,44 +49,44 @@ let alphabet_from_list l =
    "(declare-datatypes () ((A a b) (T e ea eaa eab eb)))" *)
 
 (* prefixes : string -> string list
-   renvoie la liste des préfixes d'une chaîne de caractères 
+   renvoie la liste des préfixes d'une chaîne de caractères
    Nous allons ajouter à chaque préfixe le caractère 'e'.
    Par exemple, prefixes "aba" = ["e"; "ea"; "eab"; "eaba"] *)
 let rec prefixes s =
-  (* à compléter *)
+  (* à compléter : sub de string*)
   []
-  
+
 (* prefixes_of_list : string list -> string list
-   renvoie la liste triée et sans doublons des préfixes des chaînes 
+   renvoie la liste triée et sans doublons des préfixes des chaînes
    de caractères d'une liste donnée *)
 let prefixes_of_list l =
-  (* à compléter *)
+  (* prefixes puis sort_uniq avec compare de string *)
   []
-  
+
 (* declare_types_alphabet : char list -> string
-   - prend une liste de caractères [c_1; ...; c_n] et renvoie une chaîne 
+   - prend une liste de caractères [c_1; ...; c_n] et renvoie une chaîne
    de la forme "(A c_1... c_n)" *)
 let declare_types_alphabet cl =
-  (* à compléter *)
+  (* à compléter : ? *)
   ""
 
-(* declare_types_trie : string list -> string 
-   - prend une liste l de chaînes de caractères et 
-   renvoie une chaîne de la forme "(T es_1 ... es_n)" où 
-   s_1... s_n est une énumération de tous les 
+(* declare_types_trie : string list -> string
+   - prend une liste l de chaînes de caractères et
+   renvoie une chaîne de la forme "(T es_1 ... es_n)" où
+   s_1... s_n est une énumération de tous les
    prefixes des chaînes apparaissant dans l *)
 let declare_types_trie l =
-  (* à compléter *)
+  (* à compléter : ? *)
   ""
 
-(* declare_types : string list -> char list -> string *)  
+(* declare_types : string list -> char list -> string *)
 let declare_types l cl =
-  (* à compléter *)
+  (* à compléter : ? *)
   ""
-  
+
 (* test *)
 (* Printf.printf "%s" (declare_types (li @ le) a) *)
-  
+
 
 (* ======================================================================= *)
 (* EXERCICE 3 : définir une chaîne de caractères pour les définitions
@@ -100,24 +101,24 @@ let declare_types l cl =
 let define_sorts_and_functions  =
   (* à compléter *)
   ""
-  
+
 (* ======================================================================= *)
 (* EXERCICE 4 : contraintes sur les transitions
-   La fonction assert_transition_constraints prend en entrée une trie 
-   et retourne une chaîne qui modélise les contraintes sur les transitions 
+   La fonction assert_transition_constraints prend en entrée une trie
+   et retourne une chaîne qui modélise les contraintes sur les transitions
    de l'automate décrites par la formule (51). *)
-  
+
 (* eq_trans_constr : string -> char -> string
-   - renvoi une chaîne de caractères qui correspond à une formule atomique pour 
+   - renvoi une chaîne de caractères qui correspond à une formule atomique pour
    la transition étiquetée avec 'a' à partir de l'état s
-   Par exemple, pour s = "abc" et  c = 'd' on a 
+   Par exemple, pour s = "abc" et  c = 'd' on a
    eq_trans_constr outputs "(= (f abcd)  (delta (f abc)  d))" *)
 let eq_trans_constr s a =
   (* à compléter *)
   ""
 
 (* list_transition_constraints : string list -> string list
-   prend une liste de chaînes de caractères et génère une liste 
+   prend une liste de chaînes de caractères et génère une liste
    de formules atomiques ou de chaînes vides comme suit
    - pour une chaîne non vide de la forme sa on obtient
    une chaine correspondant à l'équation f(sa)= delta (fs) a
@@ -125,13 +126,13 @@ let eq_trans_constr s a =
 let rec list_transition_constraints l =
   (* à compléter *)
   []
-  
+
 (* assert_transition_constraints : string list -> string
-   - prend en entrée une liste de mots et renvoie une chaîne qui modélise 
-   les contraintes sur les transitions de l'automate décrit par la 
+   - prend en entrée une liste de mots et renvoie une chaîne qui modélise
+   les contraintes sur les transitions de l'automate décrit par la
    formule (51).
    Par example, pour la liste [""; "ab"; "aa"; "b"] on obtient la chaîne
-   "(assert (and 
+   "(assert (and
                (= (f ea)  (delta (f e)  a))
                (= (f eaa)  (delta (f ea)  a))
                (= (f eab)  (delta (f ea)  b))
@@ -150,16 +151,16 @@ let assert_transition_constraints l =
    une chaîne de caractères qui modélise les contraintes sur les états
    acceptants décrites par la formule (52). *)
 
-(* eq_accept : string -> string 
-   - prend une chaîne de caractères s et renvoie une chaîne de caractères 
+(* eq_accept : string -> string
+   - prend une chaîne de caractères s et renvoie une chaîne de caractères
    qui modélise l'appartenance de s à l'ensemble final des états acceptants *)
 let eq_accept s =
   (* à compléter *)
   ""
 
-(* eq_non_accept : string -> string 
-   - prend une chaîne de caractères s et renvoie une chaîne de caractères 
-   qui modélise la non-appartenance de s à l'ensemble final des états acceptants 
+(* eq_non_accept : string -> string
+   - prend une chaîne de caractères s et renvoie une chaîne de caractères
+   qui modélise la non-appartenance de s à l'ensemble final des états acceptants
  *)
 let eq_non_accept s =
   (* à compléter *)
@@ -168,31 +169,31 @@ let eq_non_accept s =
 (* assert_acceptance : string list -> string list > string
    prend deux listes de chaînes de caractères, li et le, et renvoie une
    chaine qui modélise les contraintes sur les états acceptants
-   décrites par la formule (52). 
+   décrites par la formule (52).
    Les mots dans li sont acceptés et les mots dans le ne le sont pas. *)
 let assert_acceptance li le  =
   (* à compléter *)
   ""
-  
+
 (* test *)
 (* Printf.printf "%s" (assert_acceptance li le) *)
-  
+
 (* ======================================================================= *)
 (* EXERCICE 6 :
    La fonction smt_code prend en entrée deux listes de mots
-   et retourne une chaîne de caractères qui donne l'implémentation 
+   et retourne une chaîne de caractères qui donne l'implémentation
    en SMT-LIB. *)
 
-(* smt_code : string list -> string list -> string 
-   prend deux listes de chaînes de caractères, li et le, et renvoie une chaîne 
+(* smt_code : string list -> string list -> string
+   prend deux listes de chaînes de caractères, li et le, et renvoie une chaîne
    de caractères qui donne l'implémentation en SMT-LIB.
-   Les mots dans li sont acceptés et les mots dans le ne le sont pas. 
-   Pour vérifier votre algorithme, vous pouvez essayer le code SMT-LIB 
+   Les mots dans li sont acceptés et les mots dans le ne le sont pas.
+   Pour vérifier votre algorithme, vous pouvez essayer le code SMT-LIB
    que vous obtenez dans le solveur Z3: https://rise4fun.com/z3 *)
 let smt_code li le =
   (* à compléter *)
   ";; à compléter"
-  
+
 (* test *)
 (* Printf.printf "%s" (smt_code li le) *)
 
@@ -204,7 +205,7 @@ let smt_code li le =
 let input_line_opt ic =
   try Some (input_line ic)
   with End_of_file -> None
-                    
+
 let read_lines ic =
   let rec aux acc =
     match input_line_opt ic with
@@ -212,7 +213,7 @@ let read_lines ic =
     | None -> (List.rev acc)
   in
   aux []
-  
+
 let lines_of_file filename =
   let ic = open_in filename in
   let lines = read_lines ic in
@@ -223,7 +224,7 @@ let read_lists_from_file (filename: string): ((string list) * (string list))  =
   let lines = lines_of_file filename in
   (String.split_on_char ' ' (List.nth lines 0),
    String.split_on_char ' ' (List.nth lines 1))
-  
+
 let () =
   let (li,le) = (read_lists_from_file Sys.argv.(1)) in
   Printf.printf "%s" (smt_code li le)
